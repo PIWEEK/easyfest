b40b168801a546a5193fd03bf1c61cf03ba020fc
diacritica <diacritica@gmail.com>
2023-07-18 13:56:04 +0200

    up to date navbar with working routes

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,9 +2,18 @@
 
     <a href="/">Home</a>
     <a href="/about-us">About</a>
+    <a href="/accommodation-info">Accomodation info</a>
+    <a href="/code-of-conduct">Code of Conduct</a>
+    <a href="/contact-info">Contact info</a>
+    <a href="/cookie-policy">Cookie policy</a>
+    <a href="/press-kit">Press kit</a>
+    <a href="/privacy-policy">Privacy policy</a>
+    <a href="/seo">SEO</a>
+    <a href="/streaming">Streaming</a>
+    <a href="/venue-info">Venue info</a>
+
     <a href="/activities">Activities</a>
     <a href="/public-profiles">Public profiles</a>
-    <a href="/code-of-conduct">Code of Conduct</a>
 
 </nav>
 
