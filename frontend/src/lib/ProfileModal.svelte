a54f0b2749f2bad20e5c85953c41f8f6c5290145
diacritica <diacritica@gmail.com>
2023-07-20 12:10:33 +0200

    improvements on modal

--- frontend/src/lib/ProfileModal.svelte
+++ frontend/src/lib/ProfileModal.svelte
@@ -20,14 +20,29 @@
         </p>
         <button on:click={closeModal} class="delete" aria-label="close"></button>
       </header>
+      <div class="content">
       <section class="modal-card-body">
         {#if profile.attributes.bio}
         <SvelteMarkdown options={{mangle: false}} source={profile.attributes.bio}/>
         {/if}
+        {#if profile.attributes.pronouns}
+        <span class="tag">
+          {profile.attributes.pronouns}
+        </span>
+        {/if}
       </section>
+     
       <footer class="modal-card-foot">
-        El footer
+        {#if profile.attributes.activities.data.length > 0}
+        <span class="tag is-primary">
+          {#each profile.attributes.activities.data as activity}
+          <a href="/activities/{activity.id}">{activity.attributes.title}</a>
+          {/each}
+        </span>
+        {/if}
+
       </footer>
+    </div>
     </div>
   </div>
   {/if}
