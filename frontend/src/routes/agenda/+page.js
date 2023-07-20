2663f8b12d933c075387444087c7287c7118a8a8
Andrés Moya <andres.moya@kaleidos.net>
2023-07-20 13:04:12 +0200

    first real layout of agenda

--- frontend/src/routes/agenda/+page.js
+++ frontend/src/routes/agenda/+page.js
@@ -18,12 +18,57 @@ export async function load({ params }) {
             track = track.attributes
             tracks.push(track)
         }
-        console.log(tracks)
 
     } catch (e) {
         error = e
     }
+    const days = pack_activities(tracks);
     return {
-        tracks
+        days
     };
 } 
+
+/**
+ * Separate activities in days. Return an array of days,
+ * having each one a list of tracks with only the activities
+ * of this day.
+ */
+function pack_activities(tracks) {
+    const days = [];
+
+    for (let [i, track] of tracks.entries()) {
+        for (let activity of track.activities.data) {
+            const day = get_day(days, tracks, activity);
+            day.tracks[i].activities.push(activity);
+        }
+    }
+
+    return days;
+}
+
+/**
+ * Get the day for this activity. If not exists, add one and
+ * include in it all the tracks with empty activity lists.
+ */
+function get_day(days, tracks, activity) {
+    const start = new Date(activity.attributes.start);
+    for (let day of days) {
+        if (day.year === start.getFullYear() &&
+            day.month === start.getMonth() &&
+            day.date === start.getDate()) {
+            return day;
+        }
+    }
+    const day = {
+        year: start.getFullYear(),
+        month: start.getMonth(),
+        date: start.getDate(),
+        tracks: [],
+    }
+    for (let [i, track] of tracks.entries()) {
+        day.tracks.push({...track});
+        day.tracks[i].activities = [];
+    }
+    days.push(day);
+    return day;
+}
--- frontend/src/routes/agenda/+page.svelte
+++ frontend/src/routes/agenda/+page.svelte
@@ -1,39 +1,96 @@
 <script>
     import SvelteMarkdown from 'svelte-markdown'
     const storage_url = import.meta.env.VITE_STORAGE_URL
-    /** @type {import('./$types').PageData} */
     export let data;
+    console.log("data", data)
+
+    let current_day = data.days[0];
+
+    function day_click(day) {
+        current_day = day;
+    }
+
+    function activity_hour(activity) {
+        const start = new Date(activity.attributes.start);
+        return start.getHours().toString() + ":" + start.getMinutes().toString();
+    }
 </script>
 
-{#each data.tracks as track}
-    <div>{track.title} TRACK</div>
-        {#each track.activities.data as activity}
-           <div>title: {activity.attributes.title}</div>
-           <div>abstract: {activity.attributes.short_description}</div>
-           <div>start: {activity.attributes.start}</div>
-           <div>duration: {activity.attributes.minutes}</div>
-           <div>format: {activity.attributes.format}</div>
+<div class="container">
+    <div class="tabs is-toggle">
+        <ul>
+            {#each data.days as day}
+                <li class:is-active={day === current_day}>
+                    <a on:click={day_click(day)}>Day {day.date} / {day.month}</a>
+                </li>
+            {/each}
+        </ul>
+    </div>
 
-           {#if activity.attributes.attendees_limit}
-           <div>attendees limit: {activity.attributes.attendees_limit}</div>
-           {/if}
-           
-           {#if activity.attributes.needs_registration}
-           <div>This activity needs registration</div>
-           {/if}
+    <div class="columns">
+        {#each current_day.tracks as track}
+            <div class="column">
+                <h2 class="title is-3">{track.title}</h2>
+                {#each track.activities as activity}
+                    <div class="card">
+                        <div class="card-header">
+                            <p class="card-header-title">
+                                {activity_hour(activity)}
+                                {activity.attributes.title}
+                            </p>
+                        </div>
+                        {#if activity.attributes.short_description ||
+                             activity.attributes.public_faces.data.lenght > 0}
+                            <div class="card-content">
+                                {#if activity.attributes.short_description}
+                                    <p>{activity.attributes.short_description}</p>
+                                {/if}
 
-           {#if activity.attributes.has_own_page}
-           <div><a href="/activities/{activity.id}">Activity page</a></div>
-           {/if}
-               {#each activity.attributes.public_faces.data as pf}
-               {#if activity.attributes.public_faces.data}
-               By:
-               <a href="/public-profiles/{pf.id}">{pf.attributes.fullname}</a>
-               {/if}   
+                                {#each activity.attributes.public_faces.data as pf}
+                                    {#if activity.attributes.public_faces.data}
+                                        <p>
+                                            <a href="/public-profiles/{pf.id}">
+                                                {pf.attributes.fullname}
+                                            </a>
+                                        </p>
+                                    {/if}   
+                                {/each}
+                            </div>
+                        {/if}
+                    </div>
+                    <!-- <div>title: {activity.attributes.title}</div>
+                    <div>abstract: {activity.attributes.short_description}</div>
+                    <div>start: {activity.attributes.start}</div>
+                    <div>duration: {activity.attributes.minutes}</div>
+                    <div>format: {activity.attributes.format}</div>
 
-               {/each}
-           <hr/>
+                    {#if activity.attributes.attendees_limit}
+                        <div>attendees limit: {activity.attributes.attendees_limit}</div>
+                    {/if}
+                    
+                    {#if activity.attributes.needs_registration}
+                        <div>This activity needs registration</div>
+                    {/if}
 
-           {/each}
-{/each}
+                    {#if activity.attributes.has_own_page}
+                        <div><a href="/activities/{activity.id}">Activity page</a></div>
+                    {/if}
 
+                    {#each activity.attributes.public_faces.data as pf}
+                        {#if activity.attributes.public_faces.data}
+                            By:
+                            <a href="/public-profiles/{pf.id}">{pf.attributes.fullname}</a>
+                        {/if}   
+                    {/each}
+                    <hr/> -->
+                {/each}
+            </div>
+        {/each}
+    </div>
+</div>
+
+<style>
+    .card {
+        margin-bottom: 2rem;
+    }
+</style>
