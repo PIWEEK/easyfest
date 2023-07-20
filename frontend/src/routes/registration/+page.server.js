02fa1153e431c89950d53c312e63034a682628a3
diacritica <diacritica@gmail.com>
2023-07-20 16:31:49 +0200

    first registration form actions

---
+++ frontend/src/routes/registration/+page.server.js
@@ -0,0 +1,86 @@
+import axios from 'axios'
+import { z } from 'zod';
+import { fail } from '@sveltejs/kit';
+import { superValidate } from 'sveltekit-superforms/server';
+
+const base = import.meta.env.VITE_API_URL
+const path = "/site"
+   
+let data = {}
+
+const baseHeaders = {
+    "content-type": "application/json",
+    "Accept": "application/json"
+  }
+
+/**
+ * @param {string} method ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+ * @param {string} resource
+ * @param {Record<string, unknown>} [data]
+ */
+const apiClient = (method, resource, data) => {
+
+    const content = {
+      method,
+      headers: {...baseHeaders},
+    }
+    if (data) {
+      content.body = JSON.stringify(data)
+    }
+  
+      return fetch(`${base}/${resource}`, content)
+      .then(response => response.json()) //review how data is sent back
+      .catch(console.error)
+  }
+  
+
+export async function load({ params }) {
+    let fetch_site_data = {}
+    let error = null
+
+    try {
+         const res_site = await axios(base+path);
+        fetch_site_data = res_site.data.data.attributes
+
+       
+    } catch (e) {
+        error = e
+    }
+    const form = await superValidate(schema);
+    return {fetch_site_data, form};
+}
+
+// Manual schema based on STRAPI registration collection
+
+const schema = z.object({
+  fullname: z.string(),
+  email: z.string().email(),
+  title: z.string(),
+  organization: z.string(),
+  pronouns: z.string()
+
+});
+
+export const actions = {
+    default: async ({ request }) => {
+      const form = await superValidate(request, schema);
+      console.log('POST', form);
+  
+      // Convenient validation check:
+      if (!form.valid) {
+        // Again, always return { form } and things will just work.
+        return fail(400, { form });
+      }
+  
+      // TODO: Do something with the validated data
+
+      const {data} = await apiClient(
+        "POST", "registrations?populate=*",
+        { data: form.data }
+      )
+      console.log({ data: form.data })
+  
+      // Yep, return { form } here too
+      return { form };
+    }
+  };
--- frontend/src/routes/registration/+page.svelte
+++ frontend/src/routes/registration/+page.svelte
@@ -1,10 +1,68 @@
-<script>
-
+<script lang="ts">
+    import type { PageData } from './$types';
+    import { superForm } from 'sveltekit-superforms/client';
+    import SuperDebug from 'sveltekit-superforms/client/SuperDebug.svelte';
     const storage_url = import.meta.env.VITE_STORAGE_URL
     /** @type {import('./$types').PageData} */
     export let data;
-
+  
+    // Client API:
+    const { form, errors, constraints } = superForm(data.form);
 </script>
+  
+<form method="POST">
+    <label for="fullname">Full name</label>
+    <input
+      type="text"
+      name="fullname"
+      aria-invalid={$errors.fullname ? 'true' : undefined}
+      bind:value={$form.fullname}
+      {...$constraints.fullname} />
+    {#if $errors.fullname}<span class="invalid">{$errors.fullname}</span>{/if}
+  
+    <label for="email">E-mail</label>
+    <input
+      type="email"
+      name="email"
+      aria-invalid={$errors.email ? 'true' : undefined}
+      bind:value={$form.email}
+      {...$constraints.email} />
+    {#if $errors.email}<span class="invalid">{$errors.email}</span>{/if}
+
+    <label for="title">Title</label>
+    <input
+      type="text"
+      name="title"
+      aria-invalid={$errors.title ? 'true' : undefined}
+      bind:value={$form.title}
+      {...$constraints.title} />
+    {#if $errors.title}<span class="invalid">{$errors.title}</span>{/if}
+
+    <label for="title">Organization</label>
+    <input
+      type="text"
+      name="organization"
+      aria-invalid={$errors.organization ? 'true' : undefined}
+      bind:value={$form.organization}
+      {...$constraints.organization} />
+    {#if $errors.organization}<span class="invalid">{$errors.organization}</span>{/if}
+  
+    <label for="organization">Pronouns</label>
+    <input
+      type="text"
+      name="pronouns"
+      aria-invalid={$errors.pronouns ? 'true' : undefined}
+      bind:value={$form.pronouns}
+      {...$constraints.pronouns} />
+    {#if $errors.pronouns}<span class="invalid">{$errors.pronouns}</span>{/if}
+
+
+    <div><button>Submit</button></div>
+  </form>
+  
+
+
+
 <ul>
 <li>registration: {data.registration}</li>
 <li>status: {data.status}</li>
@@ -12,3 +70,9 @@
 
 </ul>
 
+<SuperDebug data={$form} />
+<style>
+    .invalid {
+      color: red;
+    }
+  </style>
