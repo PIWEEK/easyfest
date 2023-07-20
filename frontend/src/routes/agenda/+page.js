060f71080b0c9f9ea349f89515a01e8a78c17e5b
Andrés Moya <andres.moya@kaleidos.net>
2023-07-20 18:17:48 +0200

    automatic height and position of agenda cards

--- frontend/src/routes/agenda/+page.js
+++ frontend/src/routes/agenda/+page.js
@@ -3,9 +3,7 @@ import axios from 'axios'
 const base = import.meta.env.VITE_API_URL
 const path = "/tracks?populate[activities][populate][0]=public_faces"
 
-/** @type {import('./$types').PageLoad} */
 export async function load({ params }) {
-
     let tracks = []
     let track = null
     let result = []
@@ -18,10 +16,17 @@ export async function load({ params }) {
             track = track.attributes
             tracks.push(track)
         }
-
     } catch (e) {
         error = e
     }
+
+    // Ensure correct ordering of tracks and activities
+    tracks.sort((a, b) => a.order - b.order);
+    for (let track of tracks) {
+        track.activities.data.sort((a, b) => Date.parse(a.attributes.start) -
+                                             Date.parse(b.attributes.start));
+    }
+
     const days = pack_activities(tracks);
     return {
         days
@@ -39,7 +44,7 @@ function pack_activities(tracks) {
     for (let [i, track] of tracks.entries()) {
         for (let activity of track.activities.data) {
             const day = get_day(days, tracks, activity);
-            day.tracks[i].activities.push(activity);
+            add_activity(day.tracks[i], activity);
         }
     }
 
@@ -49,6 +54,8 @@ function pack_activities(tracks) {
 /**
  * Get the day for this activity. If not exists, add one and
  * include in it all the tracks with empty activity lists.
+ * 
+ * Initialize each track with the day start.
  */
 function get_day(days, tracks, activity) {
     const start = new Date(activity.attributes.start);
@@ -59,16 +66,69 @@ function get_day(days, tracks, activity) {
             return day;
         }
     }
+
     const day = {
         year: start.getFullYear(),
         month: start.getMonth(),
         date: start.getDate(),
         tracks: [],
     }
+
+    set_day_start(day, tracks);
+
     for (let [i, track] of tracks.entries()) {
         day.tracks.push({...track});
         day.tracks[i].activities = [];
+        day.tracks[i].start = day.start;
+        day.tracks[i].end = day.start;
     }
+
     days.push(day);
     return day;
+}
+
+/**
+ * Get the start time of the first activity of one day.
+ */
+function set_day_start(day, tracks) {
+    let day_start = null;
+    for (let track of tracks) {
+        for (let activity of track.activities.data) {
+            const start = new Date(activity.attributes.start);
+            if (day.year === start.getFullYear() &&
+                day.month === start.getMonth() &&
+                day.date === start.getDate()) {
+                    if (!day_start || day_start.getTime() > start.getTime()) {
+                        day_start = start;
+                    }
+                    continue;
+                }
+        }
+    }
+    day.start = day_start;
+}
+
+/**
+ * Add an activity to a track. Add a filler if there is some 
+ * hole since the previous activity, and calculate track.start{}
+ * and track.end values.
+ * 
+ * You need to call to this method in cronologic order.
+ */
+function add_activity(track, activity) {
+    const start = new Date(activity.attributes.start);
+
+    if (start.getTime() > track.end.getTime()) {
+        track.activities.push({
+            id: 0,
+            attributes: {
+                is_filler: true,
+                start: track.end,
+                minutes: (start.getTime() - track.end.getTime()) / 1000 / 60,
+            }
+        })
+    }
+
+    track.end = new Date(start.getTime() + activity.attributes.minutes * 60 * 1000);
+    track.activities.push(activity);
 }
--- frontend/src/routes/agenda/+page.svelte
+++ frontend/src/routes/agenda/+page.svelte
@@ -14,6 +14,11 @@
         const start = new Date(activity.attributes.start);
         return start.getHours().toString() + ":" + start.getMinutes().toString();
     }
+
+    function activity_height(activity) {
+        const duration = activity.attributes.minutes;
+        return (duration * 0.6 - 2).toString() + "rem";
+    }
 </script>
 
 <div class="container">
@@ -32,55 +37,69 @@
             <div class="column">
                 <h2 class="title is-3">{track.title}</h2>
                 {#each track.activities as activity}
-                    <div class="card">
-                        <div class="card-header">
-                            <p class="card-header-title">
-                                {activity_hour(activity)}
-                                {activity.attributes.title}
-                            </p>
+                    {#if activity.attributes.is_filler}
+                        <div class="filler" style="height: {activity_height(activity)}">
+                            &nbsp;
                         </div>
-                        {#if activity.attributes.short_description ||
-                             activity.attributes.public_faces.data.length > 0}
-                            <div class="card-content">
-                                {#if activity.attributes.short_description}
-                                    <p>{activity.attributes.short_description}</p>
-                                {/if}
-
-                                {#each activity.attributes.public_faces.data as pf}
-                                    {#if activity.attributes.public_faces.data}
-                                        <p>
-                                            <a href="/public-profiles/{pf.id}">
-                                                {pf.attributes.fullname}
-                                            </a>
-                                        </p>
-                                    {/if}   
-                                {/each}
-
-                                {#if activity.attributes.tag1 || activity.attributes.tag2}
-                                    <p class="tags">
-                                        {#if activity.attributes.tag1}
-                                            <span class="tag is-primary">{activity.attributes.tag1}</span>
-                                        {/if}
-                                        {#if activity.attributes.tag2}
-                                            <span class="tag is-info">{activity.attributes.tag2}</span>
-                                        {/if}
-                                    </p>
-                                {/if}
+                    {:else}
+                        <div class="card" style="height: {activity_height(activity)}">
+                            <div class="card-header">
+                                <p class="card-header-title">
+                                    {activity_hour(activity)}
+                                    {activity.attributes.title}
+                                </p>
                             </div>
-                        {/if}
-                    </div>
+                            {#if activity.attributes.short_description ||
+                                activity.attributes.public_faces.data.length > 0}
+                                <div class="card-content">
+                                    {#if activity.attributes.short_description}
+                                        <p>{activity.attributes.short_description}</p>
+                                    {/if}
+
+                                    {#each activity.attributes.public_faces.data as pf}
+                                        {#if activity.attributes.public_faces.data}
+                                            <p>
+                                                <a href="/public-profiles/{pf.id}">
+                                                    {pf.attributes.fullname}
+                                                </a>
+                                            </p>
+                                        {/if}   
+                                    {/each}
+
+                                    {#if activity.attributes.tag1 || activity.attributes.tag2}
+                                        <p class="tags">
+                                            {#if activity.attributes.tag1}
+                                                <span class="tag is-primary">{activity.attributes.tag1}</span>
+                                            {/if}
+                                            {#if activity.attributes.tag2}
+                                                <span class="tag is-info">{activity.attributes.tag2}</span>
+                                            {/if}
+                                        </p>
+                                    {/if}
+                                </div>
+                            {/if}
+                        </div>
+                    {/if}
                 {/each}
             </div>
         {/each}
     </div>
 </div>
 
-<style>
-    .card {
+<style lang="scss">
+    @use "bulma/sass/utilities/mixins.sass";
+
+    .card, .filler {
         margin-bottom: 2rem;
     }
 
     .tags {
         margin-top: 1rem;
+    }
+    
+    @include mixins.mobile {
+        .filler {
+            display: none;
+        }
     }
 </style>
