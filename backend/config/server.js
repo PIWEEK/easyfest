a950fc23cecf724ca1ff26d913d47fbfa395729e
Roberto Redradix <roberto@redradix.com>
2023-09-01 13:37:15 +0200

    chore(backend): enable local development URLs

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://api.easyfest.site',
+  url: env('PUBLIC_URL'),
   app: {
     keys: env.array('APP_KEYS'),
   },
