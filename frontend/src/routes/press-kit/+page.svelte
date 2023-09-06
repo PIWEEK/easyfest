006dca3ab0d5db07e7c1b3484912f89903be2732
Roberto Redradix <roberto@redradix.com>
2023-09-06 14:58:17 +0200

    fix: mark required model attributes by the front end

--- frontend/src/routes/press-kit/+page.svelte
+++ frontend/src/routes/press-kit/+page.svelte
@@ -8,7 +8,9 @@
     <div class="container">
         <h1 class="title">{data.title}</h1>
         <div class="content">
+          {#if data.content}
           <SvelteMarkdown options={{mangle: false}} source={data.content}/>
+          {/if}
         </div>
 
         <ul class="list">
