b18ff74da0cfd113b4b8b6826ea31634204f7dd1
Roberto Redradix <roberto@redradix.com>
2023-09-22 13:51:33 +0200

    fix(frontend): conditionally render profiles' content

--- frontend/src/routes/speakers/+page.svelte
+++ frontend/src/routes/speakers/+page.svelte
@@ -2,7 +2,7 @@
     import SvelteMarkdown from 'svelte-markdown'
     import { openModal } from 'svelte-modals'
     import Modal from "$lib/ProfileModal.svelte"
- 
+
     const storage_url = import.meta.env.VITE_STORAGE_URL
 
     /** @type {import('./$types').PageData} */
@@ -17,7 +17,9 @@
     <div class="container">
         <h1 class="title">{data.speakerscontent.title}</h1>
         <div class="content">
+          {#if data.speakerscontent.content}
           <SvelteMarkdown options={{mangle: false}} source={data.speakerscontent.content}/>
+          {/if}
 
           <div class="columns is-multiline">
             {#each data.publicprofiles as publicprofile, i}
@@ -31,14 +33,14 @@
                             {#if publicprofile.attributes.is_guest}
                             <p class="tag is-dark">Special guest</p>
                             {/if}
-                            
-          
+
+
                             <p class="title is-4">{publicprofile.attributes.fullname}
                     {#if publicprofile.attributes.nickname}
                     "{publicprofile.attributes.nickname}"
                     {/if}
                             </p>
-            
+
                     {#if publicprofile.attributes.title}
                     <p class="subtitle is-6">{publicprofile.attributes.title}</p>
                     {/if}
@@ -48,7 +50,7 @@
                 </div>
             {/each}
         </div>
-          
+
         </div>
     </div>
 </section>
