6f2b639b538e961275423741c2749475a0b0a362
Roberto Redradix <roberto@redradix.com>
2023-09-25 18:37:05 +0200

    chore(frontend): conditionally render footer page links

--- frontend/src/lib/Footer.svelte
+++ frontend/src/lib/Footer.svelte
@@ -32,17 +32,29 @@
                     {/if}
                 </a>
             </div>
+            {#if data.show_cookie_policy || data.show_privacy_policy}
             <div class="column">
                 <p class="footer-heading has-text-white">Legal</p>
+                {#if data.show_cookie_policy}
                 <p><a href="/cookie-policy" class="has-text-white">Cookie policy</a></p>
+                {/if}
+                {#if data.show_privacy_policy}
                 <p><a href="/privacy-policy">Privacy policy</a></p>
+                {/if}
             </div>
+            {/if}
             <div class="column">
                 <p class="footer-heading">EasyFest</p>
+                {#if data.show_org_team}
                 <p><a href="/org-team">Org team</a></p>
+                {/if}
                 <p><a href="/contact-info">Contact info</a></p>
+                {#if data.show_code_of_conduct}
                 <p><a href="/code-of-conduct">Code of Conduct</a></p>
+                {/if}
+                {#if data.show_press_kit}
                 <p><a href="/press-kit">Press kit</a></p>
+                {/if}
             </div>
             <div class="column">
                 <p class="footer-heading">More contacts</p>
