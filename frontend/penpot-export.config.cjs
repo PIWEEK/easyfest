b15eaccbe839a93dacb0d04708c012f8ae375d42
Roberto Redradix <roberto@redradix.com>
2023-09-28 14:41:14 +0200

    feat(frontend): allow to configure Penpot file ID

--- frontend/penpot-export.config.cjs
+++ frontend/penpot-export.config.cjs
@@ -6,12 +6,16 @@ if (typeof process.env.PENPOT_ACCESS_TOKEN !== 'string') {
 	throw new Error('Missing PENPOT_ACCESS_TOKEN environment variable');
 }
 
+if (typeof process.env.PENPOT_FILE_ID !== 'string') {
+	throw new Error('Missing PENPOT_FILE_ID environment variable');
+}
+
 const config = {
 	instance: process.env.PENPOT_BASE_URL || undefined,
 	accessToken: process.env.PENPOT_ACCESS_TOKEN,
 	files: [
 		{
-			fileId: '4a499800-872e-80e1-8002-fc0b585dc061',
+			fileId: process.env.PENPOT_FILE_ID,
 			colors: [
 				{
 					output: 'src/design-system/colors.scss',
