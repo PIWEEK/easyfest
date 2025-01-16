047aeb7a1d4054e1505b37cfc809e94a5a6cf607
Vanessa <vanessagar@gmail.com>
2025-01-16 18:32:48 +0100

    añadido page.js y llamada en page svelte para ver contenido de precios single type

--- frontend/src/routes/precios/+page.svelte
+++ frontend/src/routes/precios/+page.svelte
@@ -8,7 +8,9 @@
 <section class="section">
     <div class="container">
         <div class="content">
-
+          {#if data.content}
+          <SvelteMarkdown options={{mangle: false}} source={data.content}/>
+          {/if}
           <div class="columns ">
             <div class="column is-four-fifths">
               <p>El precio incluye pensión completa desde <strong>la cena del Jueves día 8 a la comida del Domingo día 11 inclusive</strong> en régimen de Buffet, tanto en desayuno, comida y cena, a excepción de la Cena de Gala que se celebrará la noche del Sabado en el mismo establecimiento, y se confeccionará un menú especial para la ocasión.
